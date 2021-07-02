b169d1fcff6a4fcf869279a2517c69253b7ced23
netbolo <bolomis@gmail.com>
2021-07-03 07:10:28 +0800

    init

---
+++ src/main/java/com/kiwi/spring/SpringbootPostgresCourseApplication.java
@@ -0,0 +1,13 @@
+package com.kiwi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPostgresCourseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootPostgresCourseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kiwi/spring/SpringbootPostgresCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiwi.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresCourseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
