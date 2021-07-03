6c7641fd7fb7c850263ee6d9b418905a19f40a24
netbolo <bolomis@gmail.com>
2021-07-03 09:53:18 +0800

    section5

--- src/main/java/com/kiwi/spring/SpringbootPostgresCourseApplication.java
+++ src/main/java/com/kiwi/spring/SpringbootPostgresCourseApplication.java
@@ -1,6 +1,7 @@
 package com.kiwi.spring;
 
 import com.github.javafaker.Faker;
+import com.kiwi.spring.model.Book;
 import com.kiwi.spring.model.Student;
 import com.kiwi.spring.model.StudentIdCard;
 import com.kiwi.spring.repository.StudentIdCardRepository;
@@ -10,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -35,35 +37,40 @@ public class SpringbootPostgresCourseApplication {
           email,
           faker.number().numberBetween(17, 55));
 
+      student.addBook(
+          new Book("Clean Code", LocalDateTime.now().minusDays(4)));
+
+
+      student.addBook(
+          new Book("Think and Grow Rich", LocalDateTime.now()));
+
+
+      student.addBook(
+          new Book("Spring Data JPA", LocalDateTime.now().minusYears(1)));
+
       StudentIdCard studentIdCard = new StudentIdCard(
           "123456789",
           student);
 
-      studentIdCardRepository.save(studentIdCard);
+      student.setStudentIdCard(studentIdCard);
+
+      studentRepository.save(student);
 
       studentRepository.findById(1L)
-          .ifPresent(System.out::println);
+          .ifPresent(s -> {
+            System.out.println("fetch book lazy...");
+            List<Book> books = student.getBooks();
+            books.forEach(book -> {
+              System.out.println(
+                  s.getFirstName() + " borrowed " + book.getBookName());
+            });
+          });
+//
+//            studentIdCardRepository.findById(1L)
+//                    .ifPresent(System.out::println);
 
-      studentIdCardRepository.findById(1L)
-          .ifPresent(System.out::println);
-
-      studentRepository.deleteById(1L);
+//            studentRepository.deleteById(1L);
 
     };
-  }
-
-  private void generateRandomStudents(StudentRepository studentRepository) {
-    Faker faker = new Faker();
-    for (int i = 0; i < 20; i++) {
-      String firstName = faker.name().firstName();
-      String lastName = faker.name().lastName();
-      String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
-      Student student = new Student(
-          firstName,
-          lastName,
-          email,
-          faker.number().numberBetween(17, 55));
-      studentRepository.save(student);
-    }
   }
 }
